7c47a19a00818f782613d4d34cd228ca9ee434b8
yicafe <273861905@qq.com>
2026-02-09 07:52:33 +0800

    first commit

--- src/views/Design.vue
+++ src/views/Design.vue
@@ -55,7 +55,7 @@ const products = ref([
     price: '',
     modelUrl: '../models3dgltf/model2.glb',
     imageUrl: 'https://my-strapi-project-h7zt.onrender.com/uploads/thumbnail_6loh2449_14968e26cb_b227b258d7.png', // 替换为你的预览图片
-    showModel: false,
+    showModel: true,
   },
   {
     id: 4,
@@ -64,7 +64,7 @@ const products = ref([
     price: '',
     modelUrl: '../models3dgltf/model3.glb',
     imageUrl: 'https://my-strapi-project-h7zt.onrender.com/uploads/thumbnail_6loh2449_14968e26cb_b227b258d7.png', // 替换为你的预览图片
-    showModel: false,
+    showModel: true,
   },
   {
     id: 5,
@@ -73,7 +73,7 @@ const products = ref([
     price: '',
     modelUrl: '../models3dgltf/model4.glb',
     imageUrl: 'https://my-strapi-project-h7zt.onrender.com/uploads/thumbnail_6loh2449_14968e26cb_b227b258d7.png', // 替换为你的预览图片
-    showModel: false,
+    showModel: true,
   },
   {
     id: 6,
@@ -82,7 +82,7 @@ const products = ref([
     price: '',
     modelUrl: '../models3dgltf/model5.glb',
     imageUrl: 'https://my-strapi-project-h7zt.onrender.com/uploads/thumbnail_6loh2449_14968e26cb_b227b258d7.png', // 替换为你的预览图片
-    showModel: false,
+    showModel: true,
   },
 ])
 </script>
