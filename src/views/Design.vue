d96d8dab6e3621e0f277dc7d6b9543e11ebffa07
yicafe <273861905@qq.com>
2026-02-23 21:31:23 +0800

    first commit

--- src/views/Design.vue
+++ src/views/Design.vue
@@ -2,7 +2,7 @@
   <div class="relative overflow-hidden">
     <div class="max-w-2xl px-4 py-16 mx-auto sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
       <h2 class="sr-only">Products</h2>
-      <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-2 xl:grid-cols-2 xl:gap-x-8">  <!-- 恢复用<div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"> -->
+      <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-3 xl:gap-x-8">  <!-- 恢复用<div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8"> -->
         <div v-for="product in products" :key="product.id" class="relative group" @mouseenter="product.showModel = true"
           @mouseleave="product.showModel = false">
           <a :href="product.href" class="block">
